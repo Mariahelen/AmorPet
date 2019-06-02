c94caebea0b93915ba5c9e350339cb9b53c23d38
douglas <30963374+DouglasVinicios@users.noreply.github.com>
2019-06-02 18:48:10 -0300

    Agora com controle para páginas iniciais
    Controller e links das páginas adicionado, criei também header do usuario e adm e visitante e usei o thymeleaf para adicionar eles as outras páginas de forma prática.

---
+++ AmorPet/src/main/java/com/example/demo/controllers/MenuController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MenuController {
+	@GetMapping("/home")
+	public String index() {
+		return "index";
+	}
+	@GetMapping("/termos")
+	public String termos() {
+		return "termos";
+	}
+	@GetMapping("/navegacao")
+	public String navegacao() {
+		return "navegacao";
+	}
+	@GetMapping("/quem-somos")
+	public String quemSomos() {
+		return "quemsomos";
+	}
+	@GetMapping("/login")
+	public ModelAndView login() {
+		return new ModelAndView("login");
+	}
+	@GetMapping("/cadastro")
+	public ModelAndView cadastro() {
+		return new ModelAndView("cadastro");
+	}
+	@GetMapping("/adotar")
+	public ModelAndView adotar() {
+		return new ModelAndView("quero-adotar");
+	}
+}
