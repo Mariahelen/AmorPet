34ed110055e68fe2c33e0b76e5621508ac9de22e
douglas <30963374+DouglasVinicios@users.noreply.github.com>
2019-06-27 18:38:57 -0300

    class Endereco

---
+++ AmorPet/src/main/java/com/example/demo/model/Endereco.java
@@ -0,0 +1,98 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Embeddable
+public class Endereco {
+
+	@NotBlank(message = "Campo necessário")
+	private String logradouro;
+	@NotBlank(message = "Campo necessário")
+	private int numero;
+	@NotBlank(message = "Campo necessário")
+	private String complemento;
+	@NotBlank(message = "Campo necessário")
+	private String bairro;
+	@NotBlank(message = "Campo necessário")
+	private String cidade;
+	@NotBlank(message = "Campo neecssário")
+	private List<String> estado;
+	@Pattern(regexp = "[0-9]{5}-[09]")
+	private int cep;
+	
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+	public String getComplemento() {
+		return complemento;
+	}
+	
+	
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	
+	public List<String> getEstado() {
+		return estado;
+	}
+	
+	
+	public void setEstado(List<String> estado) {
+		this.estado = estado;
+	}
+	
+	
+	public int getCep() {
+		return cep;
+	}
+	
+	
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	
+	
+}
