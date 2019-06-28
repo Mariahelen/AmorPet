0bde4fffa4b8d5084bd0b659aad5e40aad3d3311
aluno <aluno@lab03-179837>
2019-06-28 14:36:55 -0300

    Validação dos dados do cadastro do usuario pelo controller.

--- AmorPet/src/main/java/com/example/demo/controllers/UserController.java
+++ AmorPet/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,14 +1,25 @@
 package com.example.demo.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.service.UsuarioService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@GetMapping({"/perfil", "/perfil/editar"})
 	public ModelAndView exibirPerfil() {
 		return new ModelAndView("/perfil");
@@ -23,4 +34,25 @@ public class UserController {
 	public ModelAndView exibirFormDois() {
 		return new ModelAndView("/user/form-etapa-2");
 	}
+	
+	@PostMapping("/salvarCadastro")
+	public ModelAndView salvarCad(@Valid Usuario usuario, BindingResult br) {
+		ModelAndView mv = new ModelAndView("/cadastro");
+		if(br.hasErrors()) {
+			mv.addObject("usuario", usuario);
+			return mv;
+		}
+		try {
+			this.usuarioService.criarUsuario(usuario);
+			mv.addObject("concluidoCadastro", "Cadastro realizado com Sucesso !!");
+		} catch (Exception e) {
+			mv.addObject("erroCadastro", e.getMessage());
+		}
+		mv.addObject("usuario", new Usuario());
+		return mv;
+	}
+	
+	
+	
+	
 }
