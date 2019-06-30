1d146e5d0ef2049c132e6f9f9645bb69aa708af1
Douglas vinicios <douglas.vinicios1@outlook.com>
2019-06-30 17:43:33 -0300

    Função de criar conta finalizada e login sendo feito

--- AmorPet/src/main/java/com/example/demo/controllers/MenuController.java
+++ AmorPet/src/main/java/com/example/demo/controllers/MenuController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.exception.LoginInvalido;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
 
@@ -34,10 +36,29 @@ public class MenuController {
 	public String quemSomos() {
 		return "quemsomos";
 	}
+	
 	@GetMapping("/login")
-	public ModelAndView login() {
-		return new ModelAndView("login");
+	public ModelAndView login(RedirectAttributes ra) {
+		ModelAndView mv = new ModelAndView("/login");
+		if(ra.containsAttribute("mensagemError")) {
+			mv.addObject("mensagemSuccess", ra.getFlashAttributes().get("mensagemSuccess"));
+		}
+		mv.addObject("usuario", new Usuario());
+		return mv;
 	}
+	@PostMapping("/login")
+	public String efetuarLogin(Usuario usuario, RedirectAttributes ra, HttpSession session) {
+		Usuario usuarioLogado;
+		try {
+			usuarioLogado = this.usuarioService.efetuarLogin(usuario.getEmail(), usuario.getHashSenha());
+			session.setAttribute("usuarioLogado", usuarioLogado);
+		} catch (LoginInvalido e) {
+			ra.addFlashAttribute("mensagemError", e.getMessage());
+			return "redirect:/login";
+		}
+		return "redirect:/user/perfil";
+	}
+	
 	@GetMapping("/cadastro")
 	public ModelAndView cadastro(RedirectAttributes ra) {
 		ModelAndView mv = new ModelAndView("/cadastro");
--- AmorPet/src/main/java/com/example/demo/controllers/UserController.java
+++ AmorPet/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,19 +1,11 @@
 package com.example.demo.controllers;
 
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.demo.exception.LoginInvalido;
-import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
 
 @Controller
@@ -36,26 +28,6 @@ public class UserController {
 	@GetMapping("/quero-adotar/parte-2")
 	public ModelAndView exibirFormDois() {
 		return new ModelAndView("/user/form-etapa-2");
-	}
-	
-	@GetMapping({"/login"})
-	public ModelAndView login() {
-		ModelAndView mv = new ModelAndView("/index");
-		mv.addObject("usuario", new Usuario());
-		return mv;
-	}
-	
-	@PostMapping("/efetuaLogin")
-	public String efetuarLogin(Usuario usuario, RedirectAttributes ra, HttpSession session) {
-		Usuario usuarioLogado;
-		try {
-			usuarioLogado = this.usuarioService.efetuarLogin(usuario.getEmail(), usuario.getConfirmaSenha());
-			session.setAttribute("usuarioLogado", usuarioLogado);
-		} catch (LoginInvalido e) {
-			ra.addFlashAttribute("mensagemErro", e.getMessage());
-			return "redirect:/";
-		}
-		return "redirect:/user/perfil";
 	}
 	
 }
