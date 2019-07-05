44d3cf70c5fb867ce2e573eb1c527725de369fd1
lauannecsilva <47261638+lauannecsilva@users.noreply.github.com>
2019-07-05 15:58:58 -0300

    Add files via upload

--- AmorPet/src/main/java/com/example/demo/service/UsuarioService.java
+++ AmorPet/src/main/java/com/example/demo/service/UsuarioService.java
@@ -13,19 +13,20 @@ public class UsuarioService {
 
 	@Autowired
 	private UsuarioDAO usuarioDAO;
+	
 
 	public void criarUsuario(Usuario usuario) throws Exception {
 
-		if (this.usuarioDAO.findUsuarioByEmail(usuario.getEmail()) != null) {
+		if (this.usuarioDAO.findByContatoByEmail(usuario.getContato().getEmail()) != null) {
 			throw new Exception("Email já cadastrado");
 
-		} else if (!usuario.getHashSenha().equals(usuario.getConfirmaSenha())) {
+		} else if (!usuario.getSeguranca().getHashSenha().equals(usuario.getSeguranca().getConfirmaSenha())) {
 			throw new Exception("As senhas não coincidem!");
 		}
 		
 		// USUARIO ESTARÁ ATIVO AUTOMATICAMENTE POR ENQUANTO
 		// REMOVER ESTAS LINHAS ASSIM QUE TIVER ENVIO DE EMAIL
-		usuario.setAtivo(true);
+		usuario.getSeguranca().setAtivo(true);
 		
 //	    usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
 		usuarioDAO.save(usuario);
@@ -33,7 +34,7 @@ public class UsuarioService {
 
 	public Usuario efetuarLogin(String email, String senha) throws LoginInvalido {
 		Usuario usuario = this.usuarioDAO.efetuarLogin(email, senha);
-		if (usuario == null || !usuario.isAtivo()) {
+		if (usuario == null || !usuario.getSeguranca().isAtivo()) {
 			throw new LoginInvalido();
 		}
 		return usuario;
@@ -43,4 +44,5 @@ public class UsuarioService {
 		this.usuarioDAO.save(usuario);
 	}
 
+	
 }
