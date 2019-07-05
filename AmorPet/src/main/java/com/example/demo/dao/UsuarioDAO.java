82ec16b5d997b11abf596fa6a493c4a4f11d740b
lauannecsilva <47261638+lauannecsilva@users.noreply.github.com>
2019-07-05 16:00:41 -0300

    Add files via upload

--- AmorPet/src/main/java/com/example/demo/dao/UsuarioDAO.java
+++ AmorPet/src/main/java/com/example/demo/dao/UsuarioDAO.java
@@ -8,10 +8,14 @@ import com.example.demo.model.Usuario;
 
 public interface UsuarioDAO extends JpaRepository<Usuario, Integer> {
 	
-	Usuario findUsuarioByEmail(String email);
+	@Query("select u from Usuario u where u.contato.email = :email")
+	Usuario findByContatoByEmail(String email);
 	
 	// CONSULTA SE OS DADOS DO USUÁRIO CONSTAM NO BD PARA FAZER LOGIN
-	@Query("select u from Usuario u where u.email = :email and u.hashSenha = :hashSenha")
+	@Query("select u from Usuario u where u.contato.email = :email and u.seguranca.hashSenha = :hashSenha")
 	Usuario efetuarLogin(String email, String hashSenha);
 
+
+	
+	
 }
