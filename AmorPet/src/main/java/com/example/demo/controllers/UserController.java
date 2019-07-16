d4d37e9cb181e430fc1af576b2306a45d3b9bdf6
aluno <aluno@lab03-179836>
2019-07-16 18:13:12 -0300

    Ajustes no csszinho e no tempo de execucao do back do cadastrar animal

--- AmorPet/src/main/java/com/example/demo/controllers/UserController.java
+++ AmorPet/src/main/java/com/example/demo/controllers/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
